263e4279cb0ba1b5d57f4cbbc0af618ef3275539
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-04-24 09:23:21 -0300

    add login register logic

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController, Loading } from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
 /**
  * Generated class for the LoginPage page.
@@ -15,21 +16,96 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  register: boolean;
+  showRegister: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.register = false;
+  loading: Loading;
+  createSuccess = false;
+  registerCredentials = { email: '', password: '', password2: '' };
+  
+  constructor(
+    private nav: NavController, 
+    private auth: AuthServiceProvider, 
+    private alertCtrl: AlertController, 
+    private loadingCtrl: LoadingController) {
+    this.showRegister = false;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  public createAccount() {
+    this.nav.push('RegisterPage');
   }
 
-  registrar(){
-    this.register = true;
+  public login() {
+    this.showLoading()
+    this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if (allowed) {        
+        // this.nav.setRoot(HomePage);
+        this.nav.push('HomePage');
+      } else {
+        this.showError("No tiene acceso");
+      }
+    },
+      error => {
+        this.showError(error);
+      });
   }
 
-  noRegistrarse(){
-    this.register = false;
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Por favor espere...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
+
+  showError(text) {
+    this.loading.dismiss();
+ 
+    let alert = this.alertCtrl.create({
+      title: 'Falló',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+
+  public register() {
+    this.auth.register(this.registerCredentials).subscribe(success => {
+      if (success) {
+        this.createSuccess = true;
+        this.showPopup("Success", "Account created.");
+      } else {
+        this.showPopup("Error", "Problem creating account.");
+      }
+    },
+      error => {
+        this.showPopup("Error", error);
+      });
+  }
+
+  showPopup(title, text) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: text,
+      buttons: [
+        {
+          text: 'OK',
+          handler: data => {
+            if (this.createSuccess) {
+              this.nav.popToRoot();
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  showRegistration(){
+    this.showRegister = true;
+  }
+
+  hideRegistration(){
+    this.showRegister = false;
   }
 }
