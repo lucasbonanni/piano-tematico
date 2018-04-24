a6952c4fb55362b20a34859195547b3b37a6d4f3
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-04-24 09:22:40 -0300

    add auth service

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -1,5 +1,16 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+export class User {
+  name: string;
+  email: string;
+  password: string;
+ 
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+}
 
 /*
   Generated class for the AuthServiceProvider provider.
@@ -10,8 +21,52 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthServiceProvider {
 
-  constructor(public http: HttpClient) {
+  currentUser: User;
+  // public http: HttpClient
+  constructor() {
     console.log('Hello AuthServiceProvider Provider');
+    this.currentUser =  new User('','');
+  }
+
+  public login(credentials) {
+    if (credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials");
+    } else {
+      return Observable.create(observer => {
+        // At this point make a request to your backend to make a real check!
+        let access = (credentials.password === "pass" && credentials.email === "test@test.com");
+        this.currentUser = new User('Simon', 'saimon@devdactic.com');
+        observer.next(access);
+        observer.complete();
+      });
+    }
+  }
+
+  public register(credentials) {
+    if (credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials");
+    } else {
+      // At this point store the credentials to your backend!
+      return Observable.create(observer => {
+        this.currentUser.email = credentials.email;
+        this.currentUser.name = 'lucas';
+        this.currentUser.password = credentials.password;
+        observer.next(true);
+        observer.complete();
+      });
+    }
+  }
+
+  public getUserInfo() : User {
+    return this.currentUser;
+  }
+ 
+  public logout() {
+    return Observable.create(observer => {
+      this.currentUser = null;
+      observer.next(true);
+      observer.complete();
+    });
   }
 
 }
